035ab5864b73faafc891e45f24fc4ece8cc1e4c6
onikd08 <onikd08@gmail.com>
2026-03-25 07:40:17 +0200

    Add event releted routes to route index

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { UserRoutes } from "../module/user/user.route";
 import { AuthRoutes } from "../module/auth/auth.route";
+import { EventCategoryRoutes } from "../module/eventCategory/eventCategory.route";
+import { EventRoutes } from "../module/event/event.route";
+import { EventParticipationRoutes } from "../module/eventParticipation/eventParticipation.route";
 
 const router = Router();
 
-
 router.use("/users", UserRoutes);
 router.use("/auth", AuthRoutes);
+router.use("/event-categories", EventCategoryRoutes);
+router.use("/events", EventRoutes);
+router.use("/participations", EventParticipationRoutes);
 
 export const IndexRoutes = router;
