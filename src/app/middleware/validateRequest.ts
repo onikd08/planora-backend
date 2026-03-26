35af9bee3ae1fe4ed34ffc73518eac9c6f2bc9f0
onikd08 <onikd08@gmail.com>
2026-03-26 06:03:25 +0200

    Fix zod validation issues

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -1,18 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import z from "zod";
 
-export default function validateRequest(zodSchema: z.ZodObject) {
+export default function validateRequest(zodSchema: z.ZodTypeAny) {
   return (req: Request, res: Response, next: NextFunction) => {
     if (req.body.data) {
       req.body = JSON.parse(req.body.data);
     }
 
-    const result = zodSchema.safeParse(req.body);
+    const result = zodSchema.safeParse({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+    });
 
     if (!result.success) {
       next(result.error);
     } else {
-      req.body = result.data; // sanitize input
+      const data = result.data as any;
+      if (data.body) req.body = data.body;
+      if (data.query) req.query = data.query;
+      if (data.params) req.params = data.params;
 
       next();
     }
--- src/app/module/user/user.validation.ts
+++ src/app/module/user/user.validation.ts
@@ -1,24 +1,28 @@
 import { z } from "zod";
 
 const createAdminValidation = z.object({
-    firstName: z.string().min(1, "First name is required"),
-    lastName: z.string().min(1, "Last name is required"),
-    email: z.email(),
-    password: z
-    .string("Password is required")
-    .min(6, "Password must be at least 6 characters")
-    .max(20, "Password must be at most 20 characters"),
-    phone: z.string().optional(),
-    gender: z.enum(["MALE", "FEMALE", "OTHER"]).optional(),
-    profilePhoto: z.string().optional(),
+    body: z.object({
+        firstName: z.string().min(1, "First name is required"),
+        lastName: z.string().min(1, "Last name is required"),
+        email: z.email(),
+        password: z
+        .string("Password is required")
+        .min(6, "Password must be at least 6 characters")
+        .max(20, "Password must be at most 20 characters"),
+        phone: z.string().optional(),
+        gender: z.enum(["MALE", "FEMALE", "OTHER"]).optional(),
+        profilePhoto: z.string().optional(),
+    })
 });
 
 const updateProfileValidation = z.object({
-    firstName: z.string().optional(),
-    lastName: z.string().optional(),
-    phone: z.string().optional(),
-    gender: z.enum(["MALE", "FEMALE", "OTHER"]).optional(),
-    profilePhoto: z.string().optional(),
+    body: z.object({
+        firstName: z.string().optional(),
+        lastName: z.string().optional(),
+        phone: z.string().optional(),
+        gender: z.enum(["MALE", "FEMALE", "OTHER"]).optional(),
+        profilePhoto: z.string().optional(),
+    })
 });
 
 export const UserValidation = {
